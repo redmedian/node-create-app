afe3e58eb9a27b7a25f1e9c1e7d83867aa169743
RedMedian <info@redmedian.ru>
2017-03-08 20:40:59 +0300

    Configuring morgan logger and log rotation

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import bodyparser from 'body-parser';
+import morgan from 'morgan';
 import mongoose from 'mongoose';
 import bluebird from 'bluebird';
 import basicAuth from 'basic-auth-connect';
 import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
+import rfs from 'rotating-file-stream';
 
 import config from './config';
 import pageRoute from './routes/page';
@@ -15,6 +19,25 @@ mongoose.connect(config.db);
 const app = express();
 
 app.use(cors({ origin: '*' }));
+
+// Запись лога в /logs/access.log
+const logDirectory = path.join(__dirname, '../logs');
+
+// Проверяем на наличие директории logDirectory
+// eslint-disable-next-line no-unused-expressions
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+// Создаём ротацию лога
+const accessLogStream = rfs('access.log', {
+  interval: '1d', // ротировать ежедневно
+  path: logDirectory,
+});
+
+// Настройки логгера Morgan
+app.use(morgan('combined', { stream: accessLogStream }));
+
+// Вывод лога в консоль (tiny, dev, combined)
+app.use(morgan('dev'));
 
 app.use(bodyparser.json());
 
