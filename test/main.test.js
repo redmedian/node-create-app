fb1be9db07ecc3849baf8c2ea36fe03ff5cf09cd
Sergey Anischenko <info@redmedian.ru>
2017-03-17 14:39:33 +0300

    Тест системы на ответ сервера 200 OK

--- test/main.test.js
+++ test/main.test.js
@@ -1,12 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+const app = require('../app');
+const chai = require('chai');
 const request = require('supertest');
 
-const app = require('../app').default;
+const expect = chai.expect;
 
-describe('Main page', () => {
-  it('Title: Welcome', done => {
-    request(app).get('/').expect(200).expect(/Welcome/, done);
+describe('API Tests', () => {
+  it('should return version number', done => {
+    request('http://localhost:3000').get('/').end((err, res) => {
+      // eslint-disable-next-line no-unused-expressions
+      // expect(res.body.version).to.be.ok;
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
   });
 });
+
+// global.console.log('server: ', app);
 
 // Устанавливаем среду разработки в 'test'
 process.env.NODE_ENV = 'test';
